6f99f8682dc642f356e706b9ffa82b5266e866e6
reality-exe <reality@rxserver.net>
2025-04-19 01:44:52 -0400

    index page testing

--- pages/index.vue
+++ pages/index.vue
@@ -1,2 +1,21 @@
+<script setup lang="ts">
+import type { RecordModel } from 'pocketbase';
+
+let gates: RecordModel[] = [];
+const nuxtApp = useNuxtApp();
+const pb = nuxtApp.$pb;
+
+async function getGates() {
+    gates = await pb.collection("stargates").getFullList();
+}
+getGates()
+</script>
+
 <template>
+    <div>
+        <p  v-for="gate in gates">
+            {{ gate.gate_address }} => {{ gate.owner_name }}
+        </p>
+        <Button onclick="getGates()">Refresh</Button>
+    </div>
 </template>

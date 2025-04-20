cf54f2b725196c628be1c30845443c58622f943b
reality-exe <reality@rxserver.net>
2025-04-19 22:15:48 -0400

    layouts

---
+++ layouts/admin.vue
@@ -0,0 +1,66 @@
+<script lang="ts" setup>
+const nuxt = useNuxtApp();
+const pb = nuxt.$pb;
+
+const user = await pb.collection('users').authRefresh();
+</script>
+
+<template>
+  <div>
+    <SidebarProvider>
+
+      <Sidebar variant="inset">
+        <SidebarHeader>
+          <SidebarMenu>
+            <SidebarMenuItem>
+              <div class="flex gap-2">
+                <div class="flex size-10 items-center justify-center">
+                  <img src="/images/AoR_Chevron2.png" />
+                </div>
+                <div class="grid flex-1 text-left text-sm leading-tight">
+                  <span class="truncate font-semibold">Stargate Network</span>
+                  <span class="truncate text-xs">Admin Panel</span>
+                </div>
+              </div>
+            </SidebarMenuItem>
+          </SidebarMenu>
+        </SidebarHeader>
+        <SidebarContent>
+          <SidebarMenuButton as-child>
+            <a href="/admin/dashboard">
+              <Icon class="text-lg" name="mdi:view-dashboard" />
+              <span>Dashboard</span>
+            </a>
+          </SidebarMenuButton>
+          <SidebarMenuButton as-child>
+            <a href="/admin/stargates">
+              <Icon class="text-lg" name="mdi:view-list" />
+              <span>Stargates</span>
+            </a>
+          </SidebarMenuButton>
+        </SidebarContent>
+        <SidebarFooter>
+          <SidebarMenu>
+            <SidebarMenuItem>
+              <div class="flex gap-2 items-center">
+                <div class="flex size-10 items-center rounded-lg bg-accent justify-center">
+                  {{ user.record.username.slice(0, 1) }}
+                </div>
+                <div class="grid flex-1 text-left text-sm leading-tight">
+                  <span class="truncate font-semibold">{{ user.record.username }}</span>
+                  <span class="truncate text-xs">{{ user.record.email }}</span>
+                </div>
+              </div>
+            </SidebarMenuItem>
+          </SidebarMenu>
+        </SidebarFooter>
+      </Sidebar>
+      <SidebarInset>
+        <div class="p-4">
+          <slot />
+
+        </div>
+      </SidebarInset>
+    </SidebarProvider>
+  </div>
+</template>
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,7 +1,33 @@
 <script setup>
+useHead({
+    title: "Stargate Network | Gate List"
+})
+const particleOptions = {
+    fullScreen: {
+        enable: true,
+        zIndex: -1
+    },
+    particles: {
+        color: {
+            value: "#aa4fe3"
+        },
+        links: {
+            color: "#6b4191",
+            enable: true
+        },
+        move: {
+            enable: true,
+            speed: 0.25
+        },
+        number: {
+            value: 120
+        }
+    }
+};
 </script>
 
 <template>
+  <NuxtParticles id="tsparticles" :options="particleOptions" />
   <div>
     <slot />
   </div>

6a57cf97971f8b95520f30c4f2f7c40a62a7975e
reality-exe <reality@rxserver.net>
2025-04-19 22:15:44 -0400

    shadcn

---
+++ components/Star.vue
@@ -0,0 +1,30 @@
+<template>
+    <div>
+        <NuxtParticles id="tsparticles" :options="particleOptions" />
+    </div>
+</template>
+
+<script lang="ts" setup>
+const particleOptions = {
+    fullScreen: {
+        enable: true,
+        zIndex: 0
+    },
+    particles: {
+        color: {
+            value: "#aa4fe3"
+        },
+        links: {
+            color: "#6b4191",
+            enable: true
+        },
+        move: {
+            enable: true,
+            speed: 0.25
+        },
+        number: {
+            value: 120
+        }
+    }
+};
+</script>

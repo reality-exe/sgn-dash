0944970feda33f80dd909ce1ff5fe86cf82e5e8f
reality-exe <reality@rxserver.net>
2025-04-19 22:16:03 -0400

    add admin pages

---
+++ pages/admin/dashboard.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    Hai
+  </div>
+</template>
+
+<script lang="ts" setup>
+definePageMeta({
+  middleware: "admin",
+  layout: "admin"
+})
+useHead({
+  title: "SGN | Admin Dashboard"
+})
+</script>
---
+++ pages/admin/stargates.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    Hai
+  </div>
+</template>
+
+<script lang="ts" setup>
+definePageMeta({
+    middleware: "admin",
+    layout: "admin"
+})
+</script>
+
+<style>
+
+</style>

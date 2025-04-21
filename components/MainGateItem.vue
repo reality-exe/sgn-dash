c7630cd044434d5b3a2020c07771b0692d5b4a73
reality-exe <reality@rxserver.net>
2025-04-21 02:22:35 -0400

    move gate item as a dedicated component

---
+++ components/MainGateItem.vue
@@ -0,0 +1,47 @@
+<script lang="ts" setup>
+import type { RecordModel } from 'pocketbase';
+import type { Stargate } from '~/plugins/types/pocketbaseTypes';
+
+defineProps<{ gate: RecordModel }>()
+</script>
+
+<template>
+    <div>
+        <Card class="w-[345px] gap-0 p-3.5">
+            <CardHeader class="gap-1 px-0">
+                <CardTitle>
+                    <div class="flex items-center gap-2">
+                        <TooltipProvider>
+                            <Tooltip>
+                                <TooltipTrigger as-child>
+                                    <Icon :name="gate.is_headless ? 'mdi:server' : 'mdi:account'" />
+                                </TooltipTrigger>
+                                <TooltipContent>
+                                    <p>{{ gate.is_headless ? "Headless Session" : "User Session" }}</p>
+                                </TooltipContent>
+                            </Tooltip>
+                        </TooltipProvider>
+                        <p class="text-xl">{{ gate.session_name }}</p>
+                    </div>
+                </CardTitle>
+                <CardDescription>{{ gate.owner_name }}</CardDescription>
+            </CardHeader>
+            <CardContent class="px-0">
+                <div>
+                    <div class="flex justify-between">
+                        <p class="font-bold">Gate Address</p>
+                        <p>{{ gate.gate_address }}<span class="text-purple-400">{{ gate.gate_code }}</span></p>
+                    </div>
+                    <div class="flex justify-between">
+                        <p class="font-bold">Users</p>
+                        <p>{{ gate.active_users }}/{{ gate.max_users }}</p>
+                    </div>
+                    <div class="flex justify-between">
+                        <p class="font-bold">Gate Status</p>
+                        <p>{{ gate.gate_status }}</p>
+                    </div>
+                </div>
+            </CardContent>
+        </Card>
+    </div>
+</template>
--- pages/index.vue
+++ pages/index.vue
@@ -36,42 +36,7 @@ function serverIcon(b:boolean) {
         </header>
         <div class="mb-96">
             <div class="flex gap-4 p-4 flex-wrap justify-center">
-                <Card class="w-[345px] gap-0 p-3.5" v-for="(gate) in gates" :key="gate.id">
-                    <CardHeader class="gap-1 px-0">
-                        <CardTitle>
-                            <div class="flex items-center gap-2">
-                                <TooltipProvider>
-                                    <Tooltip>
-                                        <TooltipTrigger as-child>
-                                            <Icon :name="serverIcon(gate.is_headless)" />
-                                        </TooltipTrigger>
-                                        <TooltipContent>
-                                            <p>{{ gate.is_headless ? "Headless Session" : "User Session" }}</p>
-                                        </TooltipContent>
-                                    </Tooltip>
-                                </TooltipProvider>
-                                <p class="text-xl">{{ gate.session_name }}</p>
-                            </div>
-                        </CardTitle>
-                        <CardDescription>{{ gate.owner_name }}</CardDescription>
-                    </CardHeader>
-                    <CardContent class="px-0">
-                        <div>
-                            <div class="flex justify-between">
-                                <p class="font-bold">Gate Address</p>
-                                <p>{{ gate.gate_address }}<span class="text-purple-400">{{ gate.gate_code }}</span></p>
-                            </div>
-                            <div class="flex justify-between">
-                                <p class="font-bold">Users</p>
-                                <p>{{ gate.active_users }}/{{ gate.max_users }}</p>
-                            </div>
-                            <div class="flex justify-between">
-                                <p class="font-bold">Gate Status</p>
-                                <p>{{ gate.gate_status }}</p>
-                            </div>
-                        </div>
-                    </CardContent>
-                </Card>
+                <MainGateItem v-for="i in gates" :gate="i" />
             </div>
         </div>
     </div>

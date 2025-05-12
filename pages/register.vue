4a7a8572174e16d239d2696412a84adf836a08b9
reality-exe <reality@rxserver.net>
2025-05-11 23:23:46 -0400

    add register page

--- pages/register.vue
+++ pages/register.vue
@@ -1,6 +1,6 @@
 <script lang="ts" setup>
 useHead({
-  title: "Stargate Network | Login",
+  title: "Stargate Network | Register",
 });
 definePageMeta({
   middleware: "auth",
@@ -14,8 +14,10 @@ const nuxt = useNuxtApp();
 
 const formSchema = toTypedSchema(
   z.object({
-    email: z.string(),
+    username: z.string(),
+    email: z.string({ required_error: "Please input an email" }),
     password: z.string().min(6),
+    passwordConfirm: z.string().min(6)
   }),
 );
 
@@ -24,13 +26,18 @@ const form = useForm({
 });
 
 const pb = nuxt.$pb;
-const tryLogin = form.handleSubmit(async (values) => {
+const tryRegister = form.handleSubmit(async (values) => {
   pb.collection("users")
-    .authWithPassword(values.email, values.password)
+    .create({
+      username: values.username,
+      email: values.email,
+      password: values.password,
+      passwordConfirm: values.passwordConfirm
+    })
     .catch((r) => {
-      toast("Failed to login", {
+      toast("Failed to register", {
         description:
-          "Something went wrong when trying to login.\nPlease double check you typed in the corrent information.",
+          "Something went wrong when trying to register.\nPlease double check you typed in the correct information.",
         position: "top-center",
         style: {
           background: "var(--destructive)",
@@ -57,11 +64,19 @@ const passVis = ref(false);
   <div class="w-full min-h-screen flex justify-center items-center">
     <Card class="w-[512px]">
       <CardHeader>
-        <CardTitle>Login</CardTitle>
-        <CardDescription>Login to your AoR account</CardDescription>
+        <CardTitle>Register</CardTitle>
+        <CardDescription>Create AoR account</CardDescription>
       </CardHeader>
       <CardContent class="space-y-4">
-        <form @submit="tryLogin" class="space-y-4">
+        <form @submit="tryRegister" class="space-y-4">
+          <FormField v-slot="{ componentField }" name="username">
+            <FormItem>
+              <FormLabel>Username</FormLabel>
+              <FormControl>
+                <Input type="email" placeholder="some unique(probablynot) username" v-bind="componentField" />
+              </FormControl>
+            </FormItem>
+          </FormField>
           <FormField v-slot="{ componentField }" name="email">
             <FormItem>
               <FormLabel>Email</FormLabel>
@@ -87,14 +102,31 @@ const passVis = ref(false);
               <FormMessage />
             </FormItem>
           </FormField>
-          <Button type="submit" class="w-full">Login</Button>
+          <FormField v-slot="{ componentField }" name="confirmPassword">
+            <FormItem>
+              <FormLabel>Confirm Password</FormLabel>
+              <FormControl>
+                <div class="flex gap-2">
+                  <Input :type="passVis ? 'text' : 'password'" placeholder="12345678" v-bind="componentField" />
+                  <Button type="button" @click="passVis = !passVis" size="icon" variant="outline">
+                    <Icon :name="passVis
+                      ? 'mdi:eye-off'
+                      : 'mdi:eye'
+                      " />
+                  </Button>
+                </div>
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          </FormField>
+          <Button type="submit" class="w-full">Register</Button>
         </form>
         <Button class="w-full" variant="outline" type="button" @click="discordLogin()">
           <Icon class="text-lg" name="ic:round-discord" /> Continue
           with Discord
         </Button>
-        <p class="w-full text-center text-sm">Dont have an account? <a class="underline text-white"
-            href="/register">Register here</a> :3</p>
+        <p class="w-full text-center text-sm">Already have an account? <a class="underline text-white"
+            href="/login">Login here</a> :3</p>
       </CardContent>
     </Card>
   </div>
